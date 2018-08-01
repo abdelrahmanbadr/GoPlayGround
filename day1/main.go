2022b3f1148dfd9697265d53c386a7db9d87bd35
abdelrahmanbadr <abdelrahmanbadr@gmail.com>
2018-08-01 18:32:01 +0200

    day 1

---
+++ day1/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func main() {
+	x := 0
+	y := -1
+	var sum int = x + y
+	result, err := sumSlice(sum)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(result)
+	}
+	mapData()
+}
+
+func sumSlice(len int) (int, error) {
+	if len < 0 {
+		return 0, errors.New("no negative number allowed")
+	}
+	var slice = make([]int, len)
+	sum := 0
+	for i := 1; i <= len; i++ {
+		slice[i-1] = i
+	}
+	for i := 0; i < len; i++ {
+		sum += slice[i]
+	}
+	return sum, nil
+}
+
+func mapData() {
+	studentAges := make(map[string]int)
+	studentAges["st1"] = 22
+	studentAges["st2"] = 18
+	studentAges["st3"] = 20
+	for _, val := range studentAges {
+
+		fmt.Println(val)
+	}
+	delete(studentAges, "st2")
+}
